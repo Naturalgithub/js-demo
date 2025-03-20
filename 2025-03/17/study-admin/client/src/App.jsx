d24cbd4852394da71a49de91cc654264f4c202c2
v-keyuanquan <v-keyuanquan@xiaomi.com>
2025-03-20 20:50:21 +0800

    🎉 init: new-react初始化

---
+++ 2025-03/17/study-admin/client/src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+
+      <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Project name</a>
+          </div>
+          <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div class="container">
+
+        <div class="starter-template">
+          <h1>Bootstrap starter template</h1>
+          <p class="lead">Use this document as a way to quickly start any new project.All you get is this text and a mostly barebones HTML document.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
